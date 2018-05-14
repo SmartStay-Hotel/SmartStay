046d8c81d161a98b216de387c7427e2d6cf7a151
Isaac Garcia <isaacgrc94@gmail.com>
2018-05-14 20:40:32 +0200

    Creados archivos de migración para tabla rooms y rooms_types

---
+++ database/migrations/2018_05_14_173524_create_rooms_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('type_id');
+            $table->integer('capacity');
+            $table->boolean('disabled_adapted');
+            $table->boolean('jacuzzi');
+            $table->string('code', 5); //room code
+            $table->foreign('type_id')->references('id')->on('room_types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
