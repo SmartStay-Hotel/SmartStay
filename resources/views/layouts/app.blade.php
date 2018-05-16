89ef404d74081bde8d39f4e8fac5ed2fd64ead3b
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-05-16 16:35:48 +0200

    login in process - webpack.mix - VueJS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -71,5 +70,7 @@
             @yield('content')
         </main>
     </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
