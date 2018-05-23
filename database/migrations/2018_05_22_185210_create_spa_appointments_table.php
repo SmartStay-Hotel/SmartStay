de2247c467081a5ea51d71d192aa28c2a4b83178
Isaac Garcia <isaacgrc94@gmail.com>
2018-05-23 19:20:42 +0200

    Archivo migración tabla spa_appointments

---
+++ database/migrations/2018_05_22_185210_create_spa_appointments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSpaAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('spa_appointments', function (Blueprint $table) {
+            //Columns
+            $table->increments('id');
+            $table->unsignedInteger('guest_id');
+            $table->date('order_date');
+            $table->dateTime('day_hour');
+            $table->string('treatment', 50);
+            $table->integer('duration');
+            $table->double('price');
+            $table->boolean('status');
+            $table->timestamps();
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('spa_appointments');
+    }
+}
