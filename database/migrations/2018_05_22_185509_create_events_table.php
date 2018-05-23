215ae2e811976b587c6db9309759cca4642ab6f6
Isaac Garcia <isaacgrc94@gmail.com>
2018-05-23 20:25:34 +0200

    Archivo migración tabla events

---
+++ database/migrations/2018_05_22_185509_create_events_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+
+            //Columns
+            $table->increments('id');
+            $table->unsignedInteger('guest_id'); //FK
+            $table->integer('service_id');
+            $table->date('order_date');
+            $table->dateTime('day_hour');
+            $table->string('location');
+            $table->integer('max_people_num');
+            $table->double('price');
+            $table->boolean('status');
+            $table->timestamps();
+
+            //FK
+            $table->foreign('guest_id')->references('id')->on('guests');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
