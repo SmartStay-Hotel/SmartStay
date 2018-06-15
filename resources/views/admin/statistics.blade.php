9ef00484a02d4a0408bcb1d0b6445bfd6c8a1b1c
fedeBrt <federicaberti@gmail.com>
2018-06-15 11:07:35 +0200

    admin dashboard - gráfico responsive

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -7,10 +7,11 @@
     </style>
 @endsection
 @section('content')
-    <div class="card" style="box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); padding: 10px;">
+    <div class="card flex-grid" style= "box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); padding: 10px;" id="linechartparent">
     <h2 id="statisticsTitle"><i class="fas fa-calendar-alt" style="padding: 5px;"></i>Statistics</h2>
-
+        <div id="wrapper" style= " position: relative; height: 50vh;">
             <canvas id="myChart"></canvas>
+        </div>
 
     </div>
 
@@ -20,16 +21,16 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 
     <script>
-
         var canvas = document.getElementById('myChart');
         var data = {
             labels: ["January", "February", "March", "April", "May", "June", "July"],
             datasets: [
                 {
-                    label: "My First dataset",
-                    backgroundColor: "rgba(75,192,192,0.4)",
-                    borderColor: "rgba(75,192,192,1)",
-                    data: [65, 59, 80, 0, 56, 55, 40],
+                    label: "Orders",
+                    backgroundColor: "rgba(75,192,192,0.2)",
+                    borderColor: "rgba(75,192,192)",
+                    borderWidth:1,
+                    data: [6, 15, 0, 20, 12, 16, 10],
                 }
             ]
         };
@@ -44,7 +45,22 @@
         };
         var myLineChart = Chart.Bar(canvas,{
             data:data,
-            options:option
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    xAxes: [{
+                        barPercentage: 0.4,
+
+                        }],
+
+                    yAxes: [{
+                        ticks: {
+                            stepSize: 1
+                        }
+                    }]
+                }
+            }
 
         });
 
